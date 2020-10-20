4a6e6ccec92b894def19d42a9483c9d18f86a77d
mikec2020 <>
2020-10-19 21:27:03 -0400

    Adding exceptions

---
+++ RestaurantReviews.Web/Exceptions/RestaurantNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RestaurantReviews.Web
+{
+    public class RestaurantNotFoundException : Exception 
+    {
+        private readonly string _message = "Restaurant not found";
+
+        public RestaurantNotFoundException() { }
+
+        public RestaurantNotFoundException(int restaurantId)
+        {
+            _message = $"No restaurant found for ID { restaurantId }";
+        }
+
+        public override string Message => _message;
+    }
+}
---
+++ RestaurantReviews.Web/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RestaurantReviews.Web.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        private readonly string _message = "User not found";
+        
+        public UserNotFoundException() { }
+
+        public UserNotFoundException(int userId)
+        {
+            _message = $"No user found for ID { userId }";
+        }
+        
+        public override string Message => _message;
+    }
+}
