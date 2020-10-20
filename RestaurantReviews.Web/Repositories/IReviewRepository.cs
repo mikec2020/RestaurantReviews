1b2f8957dcebb0772d943b9c0a07099464df3257
mikec2020 <>
2020-10-19 21:27:17 -0400

    Adding review repository

---
+++ RestaurantReviews.Web/Repositories/IReviewRepository.cs
@@ -0,0 +1,17 @@
+﻿using RestaurantReviews.Web.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Web.Repositories
+{
+    public interface IReviewRepository
+    {
+        Task<Review> CreateReviewAsync(int restaurantId, int userId, string content);
+
+        Task<IEnumerable<Review>> GetReviewsAsync(int userId);
+
+        Task<IEnumerable<Review>> GetReviewsAsync(string username);
+
+        Task<Review> DeleteReviewAsync(int reviewId);
+    }
+}
