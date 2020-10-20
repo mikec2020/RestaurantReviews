3e1799966fd054aba16519fce818c4fd320a82b7
mikec2020 <>
2020-10-19 23:34:48 -0400

    Adding database project

---
+++ RestaurantReviewsDatabase/dbo/Tables/Users.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]    INT            IDENTITY (1, 1) NOT NULL,
+    [Username]  NVARCHAR (100) NOT NULL,
+    [FirstName] NVARCHAR (255) NOT NULL,
+    [LastName]  NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC),
+    CONSTRAINT [UK_Users] UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Username]
+    ON [dbo].[Users]([Username] ASC);
+
